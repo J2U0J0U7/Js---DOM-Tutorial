27e9ad3fbc31c18967e9c22543db2d3b1c75c19b
Julian Isaacs <126JIsaacs@frhsd.com>
2024-11-21 13:24:15 -0500

    big L

--- js/sandbox2.js
+++
@@ -1,8 +0,0 @@
-var titles = document.getElementsByClassName('title');
-
-//console.log(Array.isArray(titles));
-//console.log(Array.isArray(Array.from(titles)));
-
-Array.from(titles).forEach(function(item){
-    console.log(item);
-})
